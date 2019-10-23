2f9866ba575af485f0b52fbd962f0969a2b16ca5
yixuan <p21810@163.com>
2019-10-23 14:34:29 +0800

    update

--- src/main/java/com/sevlow/sdk/tim/api/TIMProfileService.java
+++ src/main/java/com/sevlow/sdk/tim/api/TIMProfileService.java
@@ -20,10 +20,6 @@ public interface TIMProfileService {
      */
     void setInfoGender(String identifier, GenderEnum genderEnum) throws TIMException;
 
-    /**
-     *  设置性别，年龄和学院
-     */
-    void setInfoGender(String identifier, GenderEnum genderEnum,Integer age,String collage) throws TIMException;
 
 
     /**
