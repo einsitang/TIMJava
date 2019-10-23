12385c934178d9ac5f5a01df6e5c68240f8bb1f5
yixuan <p21810@163.com>
2019-10-23 14:32:45 +0800

    update

--- src/main/java/com/sevlow/sdk/tim/api/TIMProfileService.java
+++ src/main/java/com/sevlow/sdk/tim/api/TIMProfileService.java
@@ -3,6 +3,8 @@ package com.sevlow.sdk.tim.api;
 import com.sevlow.sdk.tim.bean.profile.GenderEnum;
 import com.sevlow.sdk.tim.common.error.TIMException;
 
+import java.util.Map;
+
 /**
  * @author Element
  * @Package com.sevlow.sdk.tim.api
@@ -22,4 +24,12 @@ public interface TIMProfileService {
      *  设置性别，年龄和学院
      */
     void setInfoGender(String identifier, GenderEnum genderEnum,Integer age,String collage) throws TIMException;
+
+
+    /**
+     *  设置自定义字段和标配字段 map的key为标配或者自定义的key，
+     *      标配字段key 如 Tag_Profile_IM_Gender不能简写
+     *      自定义字段key 如 Tag_Profile_Custom_Test不能简写,需要提前在im后台设置好
+     */
+    void setInfoGender(String identifier, Map<String,String> map) throws TIMException;
 }
