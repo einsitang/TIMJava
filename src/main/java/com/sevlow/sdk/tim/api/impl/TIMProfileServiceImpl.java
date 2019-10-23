b4390aa2b54d8ed47491910fb655788776951aaa
Element <my-tangjianbin@163.com>
2019-10-23 16:33:12 +0800

    代码清理

--- src/main/java/com/sevlow/sdk/tim/api/impl/TIMProfileServiceImpl.java
+++ src/main/java/com/sevlow/sdk/tim/api/impl/TIMProfileServiceImpl.java
@@ -3,9 +3,6 @@ package com.sevlow.sdk.tim.api.impl;
 import com.sevlow.sdk.tim.api.TIMProfileService;
 import com.sevlow.sdk.tim.api.TIMService;
 import com.sevlow.sdk.tim.bean.SnsItem;
-import com.sevlow.sdk.tim.bean.chat.ChatMsgEnum;
-import com.sevlow.sdk.tim.bean.chat.MsgBody;
-import com.sevlow.sdk.tim.bean.chat.MsgContent;
 import com.sevlow.sdk.tim.bean.profile.GenderEnum;
 import com.sevlow.sdk.tim.bean.profile.TIMProfile;
 import com.sevlow.sdk.tim.common.error.TIMError;
@@ -14,15 +11,11 @@ import com.sevlow.sdk.tim.constant.TIMErrorConstant;
 import com.sevlow.sdk.tim.utils.JsonUtils;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.beanutils.BeanUtilsBean2;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang3.RandomUtils;
-import org.apache.commons.lang3.StringUtils;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author pengshiqing
@@ -81,8 +74,6 @@ public class TIMProfileServiceImpl implements TIMProfileService {
 
         if (imProfile != null) {
             try {
-//                Map<String, Object> imProfileMap = BeanUtils.describe(imProfile);
-
                 String jsonStr = JsonUtils.toJson(imProfile);
                 Map<String,Object> imProfileMap = JsonUtils.fromJson(jsonStr,new HashMap().getClass());
 
@@ -131,42 +122,4 @@ public class TIMProfileServiceImpl implements TIMProfileService {
         this.setPortrait(identifier, null, customProfile);
     }
 
-    /**
-     * 设置自定义字段和标配字段 map的key为标配或者自定义的key，
-     * 标配字段key 如Tag_Profile_IM_Gender不能简写
-     * 自定义字段key 如 Tag_Profile_Custom_Test不能简写并且需要提前在im后台设置好
-     */
-//    @Override
-//    public void setInfoGender(String identifier, Map<String, String> map) throws TIMException {
-//        if (identifier == null) {
-//            throw new TIMException(new TIMError(5000, "用户账号不存在"));
-//        }
-//
-//        if (map == null || map.keySet().size() == 0) {
-//            throw new TIMException(new TIMError(5000, "设置属性不能全部为空"));
-//        }
-//
-//        String api = "v4/profile/portrait_set";
-//
-//        Map<String, Object> body = new HashMap<>(4);
-//
-//        body.put("From_Account", identifier);
-//
-//        List<SnsItem<String>> list = new ArrayList<>();
-//
-//        // 自定义
-//        Set<String> keySet = map.keySet();
-//
-//        for (String key : keySet) {
-//            SnsItem<String> snsItem = new SnsItem<>();
-//            snsItem.setTag(key);
-//            snsItem.setValue(map.get(key));
-//            list.add(snsItem);
-//        }
-//
-//        body.put("ProfileItem", list);
-//
-//        this.timService.post(api, body);
-//
-//    }
 }
